0c1a8651f6c95d9e0cf12ceb19ac1bfada0798bf
LewiksR <luik98@gmail.com>
2024-05-26 01:10:54 +0100

    Finished words.json generating script

--- scripts/README.md
+++ scripts/README.md
@@ -2,6 +2,6 @@
 
 Uses words captured from https://github.com/dwyl/english-words
 
-Generates YAML file indexed by word length
+Generates JSON file indexed by word length
 
 Run using `python generate.py <file_name>`
--- scripts/generate.py
+++ scripts/generate.py
@@ -1,17 +1,25 @@
 from argparse import ArgumentParser
+import json
+from pprint import pprint
 
 parser = ArgumentParser(
                     prog="WordsGenerator",
-                    description="Outputs YAML file grouped by word length")
+                    description="Outputs JSON file grouped by word length")
 
 parser.add_argument("filename",
                     help="Path to input file",
                     type=str)
 parser.add_argument("-l", "--minlength",
-                    dest="minlength",
+                    dest="min_length",
                     help="Sets the minimum length of accepted words",
                     type=int,
                     default=4,
+                    required=False)
+parser.add_argument("-o", "--output",
+                    dest="output_path",
+                    help="Path to output JSON file",
+                    type=str,
+                    default="../godot/data/words.json",
                     required=False)
 
 args = parser.parse_args()
@@ -28,7 +36,7 @@ words_dict = {}
 words = load_words(args.filename)
 for word in words:
     length = len(word)
-    if length < args.minlength:
+    if length < args.min_length:
         continue
     if length not in words_dict:
         words_dict[length] = []
@@ -37,4 +45,6 @@ for word in words:
 for key in sorted(words_dict):
     print(f"Amount of words of length {key}: {len(words_dict[key])}")
 
-#yaml_string=yaml.dump(words_dict)
+words_json = json.dumps(words_dict)
+with open(args.output_path, "w") as file:
+    file.write(words_json)
